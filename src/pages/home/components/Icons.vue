6c94e808294a3ac5eae0fff7da43fdf78155379e
Mac <Mac@MacdeAir-5.lan>
2019-07-02 17:26:16 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,113 @@
+<template>
+	<div class="icons">
+		<swiper>
+		<swiper-slide v-for="(page, index) of pages" :key="index">
+		<div class="icon"
+		v-for="item of page" :key="item.id">
+			<div class="icon-img">
+			<img class="icon-img-content" :src="item.imgUrl"/>
+			</div>
+			<p class="icon-desc">{{item.desc}}</p>
+		</div>
+	</swiper-slide>
+</swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '广州必游'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }, {
+        id: '0004',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
+        desc: '珠江夜游'
+      }, {
+        id: '0005',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+        desc: '水上乐园'
+      }, {
+        id: '0006',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+        desc: '亲子游'
+      }, {
+        id: '0007',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '亲子游'
+      }, {
+        id: '0008',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/184e261814a5d07a5d3d08cd29cf590d.png',
+        desc: '长龙度假区'
+      }, {
+        id: '0009',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/c032ae43b15a3dac34b5e07bb0e46850.png',
+        desc: '广州塔'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+       .icon-desc
+         position: absolute
+         bottom: 0
+         left: 0
+         right: 0
+         height: .44rem
+         line-height: .44rem
+         color: #333
+         text-align: center
+         overflow: hidden
+         white-space: nowrap
+         text-overflow: ellipsis
+
+</style>
