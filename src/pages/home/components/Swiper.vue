9cf2f3b178b62bae890248b5118c89e58e934f52
Mac <Mac@MacdeAir-5.lan>
2019-07-02 11:28:50 +0800

    chang

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+	<swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/7ea90b7bd72642e834ced3953e231e5e.jpg_750x200_b0c66e21.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/b421363bbbb9184deacb4247eeea7485.jpg_750x200_12b42643.jpg'
+      }, {
+        id: '003',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/818f6cc784ae6669b74bbbb255414a53.jpg_750x200_66ca5873.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 26.67%
+  background: #ccc
+  .swiper-img
+    width: 100%
+</style>
