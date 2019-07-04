1d5f1c49df7157fd23d815263deff62da711d951
Mac <Mac@MacdeAir-5.lan>
2019-07-04 17:49:47 +0800

    finish city css

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,118 @@
+<template>
+	<div class="list" ref="wrapper">
+  <div>
+   <div class="area">
+    <div class="title border-topbottom">当前城市</div>
+    <div class="button-list">
+      <div class="button-wrapper"><div class="button">北京</div></div>
+   </div>
+   <div class="area">
+    <div class="title border-topbottom">热门城市</div>
+    <div class="button-list">
+      <div class="button-wrapper"><div class="button">北京</div></div>
+      <div class="button-wrapper"><div class="button">北京</div></div>
+      <div class="button-wrapper"><div class="button">北京</div></div>
+      <div class="button-wrapper"><div class="button">北京</div></div>
+   </div>
+   </div>
+   <div class="area">
+    <div class="title border-topbottom">A</div>
+    <div class="item-list">
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+    </div>
+   </div>
+   <div class="area">
+    <div class="title border-topbottom">A</div>
+    <div class="item-list">
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+    </div>
+   </div>
+   <div class="area">
+    <div class="title border-topbottom">A</div>
+    <div class="item-list">
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+    </div>
+   </div>
+   <div class="area">
+    <div class="title border-topbottom">A</div>
+    <div class="item-list">
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom">alaer</div>
+    </div>
+   </div>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scoll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+  .title
+    line-height: .44rem
+    background: #eee
+    padding-left: .2rem
+    color: #666
+    font-size: .26rem
+  .button-list
+    overflow: hidden
+    padding: .1rem .6rem .1rem .1rem
+    .button-wrapper
+      float: left
+      width: 33.33%
+      .button
+        text-align: center
+        margin: .1rem
+        padding: .1rem 0
+        border: .02rem solid #ccc
+        border-radius: .06rem
+  .item-list
+    line-height: .76rem
+    color: #666
+    padding-left: .2rem
+</style>
