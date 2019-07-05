3ecc495f046777b46e4cc24bc3efd756eef8217e
Mac <Mac@MacdeMacBook-Air-5.local>
2019-07-05 13:35:37 +0800

    ajax city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -9,54 +9,13 @@
    <div class="area">
     <div class="title border-topbottom">热门城市</div>
     <div class="button-list">
-      <div class="button-wrapper"><div class="button">北京</div></div>
-      <div class="button-wrapper"><div class="button">北京</div></div>
-      <div class="button-wrapper"><div class="button">北京</div></div>
-      <div class="button-wrapper"><div class="button">北京</div></div>
+      <div class="button-wrapper" v-for="item of hot" :key="item.id"><div class="button">{{item.name}}</div></div>
    </div>
    </div>
-   <div class="area">
-    <div class="title border-topbottom">A</div>
+   <div class="area" v-for="(item,key) of cities" :key="key">
+    <div class="title border-topbottom">{{key}}</div>
     <div class="item-list">
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-    </div>
-   </div>
-   <div class="area">
-    <div class="title border-topbottom">A</div>
-    <div class="item-list">
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-    </div>
-   </div>
-   <div class="area">
-    <div class="title border-topbottom">A</div>
-    <div class="item-list">
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-    </div>
-   </div>
-   <div class="area">
-    <div class="title border-topbottom">A</div>
-    <div class="item-list">
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
-      <div class="item border-bottom">alaer</div>
+      <div class="item border-bottom" v-for="inerItem of item" :key="inerItem.id">{{inerItem.name}}</div>
     </div>
    </div>
   </div>
@@ -68,6 +27,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scoll = new Bscroll(this.$refs.wrapper)
   }
