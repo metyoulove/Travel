eb8a958299afe74c8ec045fdeb2dcd4dc48648d5
Mac <Mac@MacdeAir-5.lan>
2019-07-08 14:32:27 +0800

    search finish

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :hot="hotCities" :cities="cities" :al="alphabet"></city-list>
     <city-alphabet
       :cities="cities"
